17b2a69b3a846acdb58094ebf9074d0c2881dc86
unknown <eslammohamedmetwally1@gmail.com>
2025-01-27 22:05:38 +0200

    create massage view

--- ChatApp/urls.py
+++ ChatApp/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
+from . import views
 
 
 
 app_name = 'chat'
 
 urlpatterns = [
-
+    path('', views.create_room, name='create_room'),
+    path('<str:room_name>/<str:username>/', views.massage_view, name='room'),
 ]
--- ChatApp/views.py
+++ ChatApp/views.py
@@ -1,10 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Room, Message
 
 
 def create_room(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        room_name = request.POST.get('room')
+
+        try:
+            get_room = Room.objects.get(name=room_name)
+        except Room.DoesNotExist:
+            new_room = Room(name=room_name)
+            new_room.save()
+        return redirect('chat:room', room_name=room_name, username=username)
     return render(request, 'index.html')
 
 
-def massage_view(request, room_id, username):
-    return render(request, 'message.html')
+def massage_view(request, room_name, username):
+    get_room = Room.objects.get(name=room_name)
+    messages = Message.objects.filter(room=get_room)
+    context = {
+        'room_name': get_room,
+        'messages': messages,
+        'user': username,
+    }
+    return render(request, 'message.html', context)
